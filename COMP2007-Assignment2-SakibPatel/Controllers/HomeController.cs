6c4989784363d8005c5d1a1d70e6306b31163690
sakib717 <sakibsabbir7@gmail.com>
2016-07-27 15:28:18 -0400

    updated assignment for "Patel Bistro" fixed the logo size to perfectly fit in the navbar and deleted default MVC home controllers to replace them with Patel Bistro material

--- COMP2007-Assignment2-SakibPatel/Controllers/HomeController.cs
+++ COMP2007-Assignment2-SakibPatel/Controllers/HomeController.cs
@@ -12,19 +12,5 @@ namespace COMP2007_Assignment2_SakibPatel.Controllers
         {
             return View();
         }
-
-        public ActionResult About()
-        {
-            ViewBag.Message = "Your application description page.";
-
-            return View();
-        }
-
-        public ActionResult Contact()
-        {
-            ViewBag.Message = "Your contact page.";
-
-            return View();
-        }
     }
 }
