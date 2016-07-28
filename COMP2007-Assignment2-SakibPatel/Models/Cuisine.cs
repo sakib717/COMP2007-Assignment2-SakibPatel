d1fd007d4e8efeb2843912dc6b5c2ae0c674c5bd
sakib717 <sakibsabbir7@gmail.com>
2016-07-28 15:40:02 -0400

    Added Browse and Index views of the "Bistro" Controller

---
+++ COMP2007-Assignment2-SakibPatel/Models/Cuisine.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace COMP2007_Assignment2_SakibPatel.Models
+{
+    public class Cuisine
+    {
+        /// <summary>
+        /// this is an empty constructor
+        /// </summary>
+        public Cuisine()
+        {
+
+        }
+
+        /// <summary>
+        /// this constructor takes one param - Name
+        /// </summary>
+        /// <param name="Name"></param>
+        
+        public Cuisine(string Name)
+        {
+            this.Name = Name; 
+        }
+
+        public string Name { get; set; }
+    }
+}
---
+++ COMP2007-Assignment2-SakibPatel/Models/Food.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace COMP2007_Assignment2_SakibPatel.Models
+{
+    public class Food
+    {
+        /// <summary>
+        /// This is the empty constructor
+        /// </summary>
+        public Food()
+        {
+
+        }
+        /// <summary>
+        /// This constructor takes one param - Title
+        /// </summary>
+        /// <param name="Title"></param>
+        
+        public Food(string Title)
+        {
+            this.Title = Title;
+        }
+
+        public string Title { get; set; }
+
+        public Cuisine Cuisine { get; set; }
+    }
+}
